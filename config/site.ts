48a5068fa2ab3a8fb60b6ce391be1cfa8b176816
Tony Papi <tonypapi028@gmail.com>
2024-08-02 01:53:47 +0000

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "EnjoyTown",
   description:
-    "Beautifully Designed website where you can watch anime, drama, movies and read mangas for free. Built with Next.JS and shadcn/ui.",
+    "Beautifully designed website where you can watch anime, drama, movies and read mangas for free. Built with Next.JS and shadcn/ui.",
   mainNav: [
     {
       title: "Drama",
@@ -26,7 +26,9 @@ export const siteConfig = {
       href: "/tv",
     },
   ],
-  links: {put
+  links: {
+    twitter: "https://twitter.com/avalynndev",
+    github: "https://github.com/avalynndev/enjoytown",
     enjoytown: "/",
   },
 };
